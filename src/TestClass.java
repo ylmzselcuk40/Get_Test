5d56f46b2b1822f70779d5fd5cf46b0dd2057928
ylmzselcuk40 <67194420+ylmzselcuk40@users.noreply.github.com>
2020-06-21 13:40:56 +0200

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den Merhaba");
-        System.out.println("Git den Merhaba");
+        System.out.println("GitHub den Merhaba");
     }
 }
